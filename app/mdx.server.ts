f61db08fa8b2ea4f3edae0aaf0534c640dbe1650
Zach Schnackel <info@zslabs.com>
2022-01-18 17:25:46 -0500

    try again

--- app/mdx.server.ts
+++ app/mdx.server.ts
@@ -2,7 +2,7 @@ import path from 'path'
 
 import { bundleMDX } from 'mdx-bundler'
 
-const postsDirectory = path.join(`${__dirname}/../../app/posts`)
+const postsDirectory = path.join(__dirname, '..', '..', 'posts')
 
 export const compileMDX = async (file: string) => {
   const fullPath = path.join(postsDirectory, `${file}.mdx`)
