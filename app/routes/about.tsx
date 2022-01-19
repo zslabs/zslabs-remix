65f1b41eceb31e8b41b272e1e6c8b5fa7adf7000
Zach Schnackel <info@zslabs.com>
2022-01-18 22:07:38 -0500

    attempt #3437840934

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,13 +1,14 @@
 import type { LoaderFunction } from 'remix'
 import { Link, useLoaderData } from 'remix'
 
-import { compileMDX } from '../mdx.server'
-
 import MDXContent from '~components/MDXContent'
 import Test from '~components/Test'
+import { compileMDX } from '~utils/mdx.server'
 
 export const loader: LoaderFunction = async () => {
-  const { code } = await compileMDX('hello-world')
+  const { code } = await compileMDX(
+    `${__dirname}/../../app/posts/hello-world.mdx`
+  )
 
   return { code }
 }
--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -2,13 +2,10 @@ import path from 'path'
 
 import { bundleMDX } from 'mdx-bundler'
 
-const postsDirectory = path.join(__dirname, '..', '..', 'posts')
-
 export const compileMDX = async (file: string) => {
-  const fullPath = path.join(postsDirectory, `${file}.mdx`)
-
   const { frontmatter, code } = await bundleMDX({
-    file: fullPath,
+    file,
+    cwd: path.dirname(file),
   })
 
   return {
