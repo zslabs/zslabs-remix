4380892bca7d43c91bc56c8f0b9870032b358532
Zach Schnackel <info@zslabs.com>
2022-02-20 07:32:20 -0500

    deps

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import { bundleMDX } from 'mdx-bundler'
 
 export const compileMDX = async (post: string) => {
-  const file = `${__dirname}/../../app/posts/${post}.mdx`
+  const file = `${__dirname}/../app/posts/${post}.mdx`
 
   const { frontmatter, code } = await bundleMDX({
     file,
